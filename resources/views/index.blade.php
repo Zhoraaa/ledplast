d970331f59981adda0b1bf4322cca70608b98545
zh0raaa <102014678+Zhoraaa@users.noreply.github.com>
2023-09-27 10:04:31 +0500

    0.0.2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layout')
 
 @section('body')
-    
+    <main>
+      <form action="" method="post">
+        <input type="text" placeholder="Название статьи">
+        <textarea name="" id="" cols="30" rows="10" placeholder="Текст статьи">
+          
+        </textarea>
+      </form>
+    </main>
 @endsection
