091c10a033921c7bb8216ab10fd4cddd6b5fdf8f
SenyaTok <123239680+SenyaTok@users.noreply.github.com>
2023-10-02 13:56:06 +0500

    0.0.1T Редактор статей

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,25 @@
 @extends('layout')
 
 @section('body')
-<<<<<<< HEAD
     <main>
       <form action="" method="post">
         <input type="text" placeholder="Название статьи">
         <textarea name="" id="" cols="30" rows="10" placeholder="Текст статьи">
-          
         </textarea>
+        <script>
+          tinymce.init({
+            selector: 'textarea',
+            plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Author name',
+            mergetags_list: [
+              { value: 'First.Name', title: 'First Name' },
+              { value: 'Email', title: 'Email' },
+            ],
+            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant"))
+          });
+        </script>
       </form>
     </main>
-=======
-    bfgfgfgfgf
->>>>>>> e26842764e50917dad94e7fd0007c730d1c8f591
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,10 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ledplast - Уральский светотехнический завод</title>
     <link rel="stylesheet" href="../css/style.css">
+    <script src="https://cdn.tiny.cloud/1/wegmd8l3rnqmlxgs6n1fnjamuvdy1ki98y9nrwvzyukkbva5/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
 
 <body>
-    @yield('body');
+    @yield('body')
 </body>
 
 </html>
