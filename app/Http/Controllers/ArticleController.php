fea1f1345c025b1158777f028074cec3b8355afa
SenyaTok <123239680+SenyaTok@users.noreply.github.com>
2023-10-09 13:27:40 +0500

    0.0.2T Продолжение 2

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,18 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\ArticleType;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    // public function index()
-    // {
-    //     $courses = Article::all();
+    public function index()
+    {
+        $articles = Article::all();
 
-    //     return view("index",[
-    //         "all_articles"=>$articles
-    //     ]);
+        return view("index",[
+            "all_articles"=>$articles
+        ]);
         
-    // }
-    // Продолжу А
+    }
+
+    public function create_article(Request $request)
+    {
+
+        $request->validate([
+            "title" => "required|max:50",
+            "description" => "required",
+            "cost" => "required|numeric",
+            "duration" => "required|digits:value",
+            "category_id" => "required",
+
+        ],[ 
+            "title.required"=>"Поле обязательно для заполнения",
+            "description.required"=>"Поле обязательно для заполнения",
+            "cost.required"=>"Поле обязательно для заполнения",
+            "duration.required"=>"Поле обязательно для заполнения",
+            "category_id.required"=>"Поле обязательного заполнения",
+            
+            "title.max"=>"Название курса должно содержать не больше 50 символов",
+            
+            "cost.numeric"=>"Цена должна состоять из цифр",
+
+        ]);
+
+        $article_info = $request->all();
+
+        Article::create([
+            "title"=> $article_info["title"],
+            "description"=> $article_info["description"],
+            "cost"=> $article_info["cost"],
+            "duration"=> $article_info["duration"],
+            "category_id"=> $article_info["category"],
+
+        ]);
+
+        return redirect("/");
+    }
 }
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        "name",
+        "text",
+        "image",
+        "article_type_id"
+    ];
 }
